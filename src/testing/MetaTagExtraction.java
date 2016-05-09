7f56cec8cf231d0e4ae1c1f688506063c9eab622
boyraz <filiz.boyraz@hotmail.com>
2016-05-09 11:58:56 +0200

    Update MetaTagExtraction.java

--- src/testing/MetaTagExtraction.java
+++ src/testing/MetaTagExtraction.java
@@ -21,7 +21,6 @@ public class MetaTagExtraction {
     private static HashSet<String> unusedData = new HashSet<String>();
     private static HashMap<String, Integer> usedTags = new HashMap<String, Integer>();
 
-    // TODO replace file locations
     private final static String IMAGES_PATH = "C:/Users/Filiz/Downloads/python-crawler/images/";
     private final static String BLANK_IMAGE_PATH = "C:/Users/Filiz/Downloads/python-crawler/images/0102164002.jpg";
 
@@ -31,8 +30,11 @@ public class MetaTagExtraction {
     public static void main(String[] args) {
         try {
             Connection c = connect("jdbc:sqlite:images.db");
+            System.out.println("connected");
             getAllTags(c);
+            System.out.println("alltags");
             reduceTagsInDB(c);
+            System.out.println("reduce");
             c.close();
         } catch (Exception e) {
             System.err.println(e.getClass() + " " + e.getMessage());
@@ -45,7 +47,7 @@ public class MetaTagExtraction {
         return DriverManager.getConnection(db);
     }
 
-    private static void getAllTags(Connection c) throws SQLException {
+    private static void getAllTags(Connection c) throws SQLException, IOException {
         String query = "SELECT Id, Metadata FROM Images";
         Statement stmt = c.createStatement();
         ResultSet rs = stmt.executeQuery(query);
@@ -64,8 +66,7 @@ public class MetaTagExtraction {
         }
     }
 
-    private static boolean usingPicture(String picName) {
-        try {
+    private static boolean usingPicture(String picName) throws IOException {
             String thisImagePath = IMAGES_PATH + picName + ".jpg";
             BufferedImage thisImage = ImageIO.read(new File(thisImagePath));
             BufferedImage blankImage = ImageIO.read(new File(BLANK_IMAGE_PATH));
@@ -73,9 +74,6 @@ public class MetaTagExtraction {
                 unusedData.add(picName);
                 return false;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         return true;
     }
 
@@ -97,21 +95,35 @@ public class MetaTagExtraction {
         ResultSet rs = stmt.executeQuery(query);
         HashSet<Integer> nrOfTags = new HashSet<Integer>();
         Connection outfitter = connect("jdbc:sqlite:outfitterdb.db");
+        resetTables(outfitter);
         while(rs.next()) {
             if(!unusedData.contains(rs.getString("Id"))) {
+                System.out.println("CHECK");
                 HashSet<String> usefulTags = choseUsefulTags(rs.getString("Metadata").split(" "));
                 nrOfTags.add(usefulTags.size());
                 String pid = rs.getString("Id");
-                insert(outfitter, "insert into product values('" + pid + "','" + rs.getString("Top") + "','" + rs.getString("Gender") + "');");
+                insert(outfitter, "insert into product values('" + pid + "','" + rs.getString("Top") + "');");
+                addProductTag(outfitter, rs.getString("Gender"), pid);
                 for(String tag : usefulTags) {
                     addProductTag(outfitter, tag, pid);
                 }
             }
         }
         outfitter.close();
+        System.out.println("Nr of unique tags: " + usedTags.size());
         System.out.println("Number of unused clothes: " + unusedData.size());
         System.out.println("Max: "+ Collections.max(nrOfTags));
         System.out.println("Min: "+ Collections.min(nrOfTags));
+    }
+
+    private static void resetTables(Connection c) throws SQLException{
+        String product = "DELETE FROM Product";
+        String productTag = "DELETE FROM productTag";
+        String tags = "DELETE FROM tags";
+        Statement stmt = c.createStatement();
+        stmt.executeUpdate(product);
+        stmt.executeUpdate(productTag);
+        stmt.executeUpdate(tags);
     }
 
     private static void addProductTag(Connection c, String tag, String pid) {
